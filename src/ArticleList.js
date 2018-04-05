f5bd8517d3b7b75b437a743f110385a2ff229f28
Edward <ea.litten@gmail.com>
2018-04-05 23:32:52 +0100

    Add ajax to load articles in ArticleList

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -5,6 +5,34 @@ import Article from './Article';
 // const savedArticles = savedResponse.response.results
 
 class ArticleList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      articles: [],
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/saved_response.json")
+      .then(response => response.json())
+      .then(
+        (data) => {
+          this.setState({
+            isLoaded: true,
+            articles: data.response.results
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    }
+
   renderArticles() {
     return this.state.articles.map((article) => {
       return (
@@ -14,14 +42,23 @@ class ArticleList extends Component {
       );
     })
   }
+
   render() {
+    const error = this.state.error;
+    const isLoaded = this.state.isLoaded;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
       return (
         <div>
         <h2>The latest tech news from the Guardian</h2>
-    
+
         <ul className="article-list">{this.renderArticles()}</ul>
-      </div>
+        </div>
       )
+    }
   }
 }
 
